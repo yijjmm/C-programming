a8b2d7200a4f983c42faeb4217de9f0e56a68a43
Jimin Lee <47144126+yijjmm@users.noreply.github.com>
2019-02-03 15:31:01 +0900

    Create 반복문 연습3.c

---
+++ 반복문 연습3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i;
+    for(i = 1; i < 101; i++)
+    {
+        if(i % 2 == 1)
+        {
+            printf("0");
+        }d
+        else
+        {
+            printf("1");
+        }
+    }
+    getch();
+}
