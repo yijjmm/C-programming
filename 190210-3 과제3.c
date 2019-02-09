634415899dd138430bef36752e8af5250920e12b
Jimin Lee <47144126+yijjmm@users.noreply.github.com>
2019-02-10 02:45:33 +0900

    Create 190210-3 과제3.c

---
+++ 190210-3 과제3.c
@@ -0,0 +1,22 @@
+// 어떤 자연수 n을 입력 받고, 1~n까지 자연수 중 홀수의 제곱의 합 출력
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i;
+    int a;
+    int sum = 0;
+
+    scanf("%d", &a);
+
+    for(i = 1; i <= a; i++)
+    {
+        if(i % 2 == 1)
+        {
+            sum += i*i;
+        }
+    }
+    printf("%d", sum);
+    getch();
+}
