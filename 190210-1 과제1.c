9eb8b167dd4c0bb9bea74637f86524f87e293ca9
Jimin Lee <47144126+yijjmm@users.noreply.github.com>
2019-02-10 01:39:31 +0900

    Create 190210-1 과제1.c

---
+++ 190210-1 과제1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int sum = 0;
+    int i;
+
+    for(i = 1; i < 101; i++)
+    {
+        sum += i*i;
+    }
+    printf("%d", sum);
+    getch();
+}
