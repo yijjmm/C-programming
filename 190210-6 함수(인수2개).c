2ff7e6daf28f4c8853408888f8b7a9ab4ad73f87
Jimin Lee <47144126+yijjmm@users.noreply.github.com>
2019-02-10 15:05:49 +0900

    Update 190210-6 함수(인수2개).c

--- 190210-6 함수(인수2개).c
+++ 190210-6 함수(인수2개).c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<conio.h>
 
-int sum(int a, int b)
+int sum(int a, int b) // 인수를 2개로 설정
 {
     int c = a + b;
     return c;
@@ -9,6 +9,6 @@ int sum(int a, int b)
 
 int main()
 {
-    printf("%d", sum(5, 10));
+    printf("%d", 2 * sum(5, 10)); // 함수 값을 2배 시켜줌
     getch();
 }
